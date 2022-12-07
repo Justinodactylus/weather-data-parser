03d2dbe9d54898dde7b6b634a8e1908c8975acf9
Justin Kreller <justin.kreller@stud.htwk-leipzig.de>
2022-12-07 16:39:07 +0100

    refactor some code and add function to download file (not yet working)

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
-import urllib
-import os
-from webbrowser import open_new
-import requests
+import ftplib
+import urllib.request
 import csv
 import argparse
-from bs4 import BeautifulSoup
 
-HOURLY_URL='https://www.ncei.noaa.gov/pub/data/uscrn/products/hourly02/'
-DAILY_URL='https://www.ncei.noaa.gov/pub/data/uscrn/products/daily01/'
-MONTHLY_URL='https://www.ncei.noaa.gov/pub/data/uscrn/products/monthly01/'
+HOSTNAME='ftp.ncdc.noaa.gov'
+HOURLY_PATH='/pub/data/uscrn/products/hourly02/'
+DAILY_PATH='/pub/data/uscrn/products/daily01/'
+MONTHLY_PATH='/pub/data/uscrn/products/monthly01/'
+CREDENTIALS = ["ftp", "tomiger450\@dmonies.com"] # random email
 
 def cliArgumentParser():
     argParser = argparse.ArgumentParser(description='A parser to collect weather data and converts it into a XML schema.')
@@ -18,20 +17,16 @@ def cliArgumentParser():
 
     return argParser.parse_args()
 
-def getUrlPaths(url, ext='', params={}):
-    response = requests.get(url, params=params)
+def getUrlPaths(url: str, path: str, credentials: list):
+    f = ftplib.FTP(url)
+    f.login(credentials[0], credentials[1])
+    f.cwd(path)
+    ls = []
+    f.retrlines('MLSD', ls.append)
+    #callback=lambda line: ls.append(str(line).rpartition(';')[2].removeprefix(' ')))
+    return ls
 
-    if response.ok:
-        responseText = response.text
-    else:
-        return response.raise_for_status()
-    
-    soup = BeautifulSoup(responseText, 'html.parser')
-    parent = [str(url + node.get('href')) for node in soup.find_all('a') if node.get('href').endswith(ext)]
-
-    return parent
-
-def getUrl(time='hourly', year=2022, location_vector='3_S', ext='.txt'):
+def getUrl(time=None, year=None, location_vector=None, ext=None):
     stations = dict()
     with open("resources/stations.tsv") as tsv_file:
         stations_reader = csv.reader(tsv_file, delimiter="\t")
@@ -41,28 +36,36 @@ def getUrl(time='hourly', year=2022, location_vector='3_S', ext='.txt'):
             station_vector_list.append(list[4].replace(' ', '_')) # gets all state location vectors from stations.tsv and replace all spaces with '_' 
 
         intervallUrl = None
-        finalUrl = None
+        finalPath = None
         match time:
-            case "hourly": intervallUrl = HOURLY_URL
-            case "daily": intervallUrl = DAILY_URL
+            case "hourly": intervallUrl = HOURLY_PATH
+            case "daily": intervallUrl = DAILY_PATH
             case "monthly": 
-                return getPaths(MONTHLY_URL, ext, location_vector + ext)
+                return HOSTNAME + MONTHLY_PATH + getPath(HOSTNAME, MONTHLY_PATH, location_vector + ext)
             case _: return None
 
-        yearUrl = getPaths(intervallUrl, '/', str(year) + '/')
-        finalUrl = getPaths(yearUrl, ext, location_vector + ext)
-        return finalUrl
+        yearPath = getPath(HOSTNAME, intervallUrl, str(year))
+        finalPath = getPath(HOSTNAME, intervallUrl + yearPath, location_vector + ext)
+        return HOSTNAME + intervallUrl + yearPath + finalPath
 
-def getPaths(url: str, ext: str, endsWith: str):
-    urlList = getUrlPaths(url, ext)
+def getPath(hostname: str, path: str, endsWith: str):
+    urlList = getUrlPaths(hostname, path, CREDENTIALS)
     for ur in urlList:
+        ur = str(ur)
+        #print(ur)
         if (ur.endswith(endsWith)):
-            return ur
-    raise Exception("No path '" + url + endsWith + "' found.") 
+            return ur.rpartition(';')[2].removeprefix(' ') + '/'
+    raise Exception("No path '" + hostname + path + '*' + endsWith + "*' found.")
+
+def getFile(urlPath: str, filename: str, credentials: list):
+    #print('ftp://' + credentials[0] + ':' + credentials[1] + '@' + urlPath + filename)
+    #urllib.request.urlretrieve('ftp://' + credentials[0] + ':' + credentials[1] + '@' + urlPath + filename, 'tmp.txt')
+    urllib.request.urlretrieve('ftp://ftp:tomiger450\@dmonies.com@ftp.ncdc.noaa.gov/pub/data/uscrn/products/daily01/2022/CRND0103-2022-AK_Deadhorse_3_S.txt', 'CRND0103-2022-AK_Deadhorse_3_S.txt')
 
 def main():
     args = cliArgumentParser()
-    getUrl(time=args.time_intervall, year=args.year, location_vector=args.location, ext='.txt')
+    finalUrl = getUrl(time=args.time_intervall, year=args.year, location_vector=args.location, ext='.txt')
+    getFile(finalUrl.rpartition('/')[0], finalUrl.rpartition('/')[2], CREDENTIALS)
 
 if __name__ == "__main__":
     main()
