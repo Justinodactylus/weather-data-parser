f2910ff18bb4d78c41d1a5c56818f54da612b980
Justin Kreller <justin.kreller@stud.htwk-leipzig.de>
2022-12-22 22:39:27 +0100

    Refactor some methods

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import ftplib
-import urllib.request
 import csv
 import argparse
 
@@ -9,24 +8,41 @@ DAILY_PATH='/pub/data/uscrn/products/daily01/'
 MONTHLY_PATH='/pub/data/uscrn/products/monthly01/'
 CREDENTIALS = ["ftp", "tomiger450\@dmonies.com"] # random email
 
+def parseCredentials(string):
+    global CREDENTIALS
+    string = str(string)
+    strTuple = string.partition(':')
+
+    if (strTuple[1] == ''):
+        raise Exception("The given credentials don't have the right format USERNAME:PASSWORD.")
+    CREDENTIALS = [strTuple[0], strTuple[2]]
+
 def cliArgumentParser():
     argParser = argparse.ArgumentParser(description='A parser to collect weather data and converts it into a XML schema.')
     argParser.add_argument("-t", "--time-intervall", type=str, choices=["hourly", "daily", "monthly"], required=True)
-    argParser.add_argument("-y", "--year", type=int, required=False)
+    argParser.add_argument("-y", "--year", type=int, choices=range(2000, 2023), required=False)
     argParser.add_argument("-l", "--location", type=str, help="The state location vector from the stations.tsv file.", required=True)
+    argParser.add_argument("-c", "--credentials", type=parseCredentials, help="The credentials to login to a FTP server, seperated by a ':'.", required=False)
 
     return argParser.parse_args()
 
 def getUrlPaths(url: str, path: str, credentials: list):
-    f = ftplib.FTP(url)
-    f.login(credentials[0], credentials[1])
-    f.cwd(path)
+    ftp = ftplib.FTP(url)
+    ftp.login(credentials[0], credentials[1])
+    ftp.cwd(path)
     ls = []
-    f.retrlines('MLSD', ls.append)
-    #callback=lambda line: ls.append(str(line).rpartition(';')[2].removeprefix(' ')))
+    ftp.retrlines('MLSD', ls.append)
     return ls
 
-def getUrl(time=None, year=None, location_vector=None, ext=None):
+def getPath(hostname: str, path: str, endsWith: str, credentials: list):
+    urlList = getUrlPaths(hostname, path, credentials)
+    for ur in urlList:
+        ur = str(ur)
+        if (ur.endswith(endsWith)):
+            return ur.rpartition(';')[2].removeprefix(' ') + '/'
+    raise Exception("No path '" + hostname + path + '*' + endsWith + "*' found.")
+
+def getUrl(time: str, year: int, location_vector: str, ext: str, credentials: list):
     stations = dict()
     with open("resources/stations.tsv") as tsv_file:
         stations_reader = csv.reader(tsv_file, delimiter="\t")
@@ -36,36 +52,47 @@ def getUrl(time=None, year=None, location_vector=None, ext=None):
             station_vector_list.append(list[4].replace(' ', '_')) # gets all state location vectors from stations.tsv and replace all spaces with '_' 
 
         intervallUrl = None
-        finalPath = None
         match time:
             case "hourly": intervallUrl = HOURLY_PATH
             case "daily": intervallUrl = DAILY_PATH
             case "monthly": 
-                return HOSTNAME + MONTHLY_PATH + getPath(HOSTNAME, MONTHLY_PATH, location_vector + ext)
+                return HOSTNAME + MONTHLY_PATH + getPath(HOSTNAME, MONTHLY_PATH, location_vector + ext, credentials).removesuffix('/')
             case _: return None
 
-        yearPath = getPath(HOSTNAME, intervallUrl, str(year))
-        finalPath = getPath(HOSTNAME, intervallUrl + yearPath, location_vector + ext)
+        yearPath = getPath(HOSTNAME, intervallUrl, str(year), credentials)
+        finalPath = getPath(HOSTNAME, intervallUrl + yearPath, location_vector + ext, credentials).removesuffix('/')
         return HOSTNAME + intervallUrl + yearPath + finalPath
 
-def getPath(hostname: str, path: str, endsWith: str):
-    urlList = getUrlPaths(hostname, path, CREDENTIALS)
-    for ur in urlList:
-        ur = str(ur)
-        #print(ur)
-        if (ur.endswith(endsWith)):
-            return ur.rpartition(';')[2].removeprefix(' ') + '/'
-    raise Exception("No path '" + hostname + path + '*' + endsWith + "*' found.")
+def getFile(url: str, credentials: list):
+    hostname = url.partition('/')[0]
+    path = '/' + url.partition('/')[2].rpartition('/')[0] + '/'
+    filename = url.rpartition('/')[2]
 
-def getFile(urlPath: str, filename: str, credentials: list):
-    #print('ftp://' + credentials[0] + ':' + credentials[1] + '@' + urlPath + filename)
-    #urllib.request.urlretrieve('ftp://' + credentials[0] + ':' + credentials[1] + '@' + urlPath + filename, 'tmp.txt')
-    urllib.request.urlretrieve('ftp://ftp:tomiger450\@dmonies.com@ftp.ncdc.noaa.gov/pub/data/uscrn/products/daily01/2022/CRND0103-2022-AK_Deadhorse_3_S.txt', 'CRND0103-2022-AK_Deadhorse_3_S.txt')
+    # Connect to FTP Server
+    ftp_server = ftplib.FTP(hostname)
+
+    # Login with credentials
+    ftp_server.login(credentials[0], credentials[1])
+
+    # Change current directory
+    ftp_server.cwd(path)
+
+    # Start download and write it to a file
+    ftp_server.retrbinary("RETR " + filename, open(filename, 'wb').write)
+    
+    # Close the Connection
+    ftp_server.quit()
 
 def main():
     args = cliArgumentParser()
-    finalUrl = getUrl(time=args.time_intervall, year=args.year, location_vector=args.location, ext='.txt')
-    getFile(finalUrl.rpartition('/')[0], finalUrl.rpartition('/')[2], CREDENTIALS)
+    
+    finalUrl = getUrl(args.time_intervall, args.year, args.location, '.txt', CREDENTIALS)
+
+    print("Following file was found: '" + finalUrl + "'\n\nStarting download ...\n")
+
+    getFile(finalUrl, CREDENTIALS)
+
+    print("Download of file finished.\n\n")
 
 if __name__ == "__main__":
     main()
