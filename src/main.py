96f0c128ffce75e466276f42d172cd2b745764fb
Justin Kreller <justin.kreller@stud.htwk-leipzig.de>
2022-12-29 01:41:50 +0100

    add functionality to read in data from a file and write this data with specific headers for this data in a set xml-elements-tag mapping to a xml templates file

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
+from fileinput import filename
+from __const__ import *
 import ftplib
 import csv
 import argparse
+import lxml.etree as ET
+from copy import deepcopy
 
-HOSTNAME='ftp.ncdc.noaa.gov'
-HOURLY_PATH='/pub/data/uscrn/products/hourly02/'
-DAILY_PATH='/pub/data/uscrn/products/daily01/'
-MONTHLY_PATH='/pub/data/uscrn/products/monthly01/'
-CREDENTIALS = ["ftp", "tomiger450\@dmonies.com"] # random email
+INTERVALL_PATH = ''
 
 def parseCredentials(string):
     global CREDENTIALS
@@ -16,6 +16,14 @@ def parseCredentials(string):
     if (strTuple[1] == ''):
         raise Exception("The given credentials don't have the right format USERNAME:PASSWORD.")
     CREDENTIALS = [strTuple[0], strTuple[2]]
+
+def setTimeIntervall(timeIntervall: str):
+    global INTERVALL_PATH
+    match timeIntervall:
+            case "hourly": INTERVALL_PATH = HOURLY_PATH
+            case "daily": INTERVALL_PATH = DAILY_PATH
+            case "monthly": INTERVALL_PATH = MONTHLY_PATH 
+            case _: return None
 
 def cliArgumentParser():
     argParser = argparse.ArgumentParser(description='A parser to collect weather data and converts it into a XML schema.')
@@ -27,11 +35,15 @@ def cliArgumentParser():
     return argParser.parse_args()
 
 def getUrlPaths(url: str, path: str, credentials: list):
+    if (url.startswith("http://") or url.startswith("https://")):
+        url = url.partition('//')[2]
+
     ftp = ftplib.FTP(url)
     ftp.login(credentials[0], credentials[1])
     ftp.cwd(path)
     ls = []
     ftp.retrlines('MLSD', ls.append)
+    ftp.quit()
     return ls
 
 def getPath(hostname: str, path: str, endsWith: str, credentials: list):
@@ -51,19 +63,17 @@ def getUrl(time: str, year: int, location_vector: str, ext: str, credentials: li
         for list in stations_reader:
             station_vector_list.append(list[4].replace(' ', '_')) # gets all state location vectors from stations.tsv and replace all spaces with '_' 
 
-        intervallUrl = None
-        match time:
-            case "hourly": intervallUrl = HOURLY_PATH
-            case "daily": intervallUrl = DAILY_PATH
-            case "monthly": 
-                return HOSTNAME + MONTHLY_PATH + getPath(HOSTNAME, MONTHLY_PATH, location_vector + ext, credentials).removesuffix('/')
-            case _: return None
+        if time == 'monthly' and INTERVALL_PATH:
+            return HOSTNAME + INTERVALL_PATH + getPath(HOSTNAME, MONTHLY_PATH, location_vector + ext, credentials).removesuffix('/')
 
-        yearPath = getPath(HOSTNAME, intervallUrl, str(year), credentials)
-        finalPath = getPath(HOSTNAME, intervallUrl + yearPath, location_vector + ext, credentials).removesuffix('/')
-        return HOSTNAME + intervallUrl + yearPath + finalPath
+        yearPath = getPath(HOSTNAME, INTERVALL_PATH, str(year), credentials)
+        finalPath = getPath(HOSTNAME, INTERVALL_PATH + yearPath, location_vector + ext, credentials).removesuffix('/')
+        return HOSTNAME + INTERVALL_PATH + yearPath + finalPath
 
-def getFile(url: str, credentials: list):
+def getFile(url: str, credentials: list): 
+    if (url.startswith("http://") or url.startswith("https://")):
+        url = url.partition('//')[2]
+
     hostname = url.partition('/')[0]
     path = '/' + url.partition('/')[2].rpartition('/')[0] + '/'
     filename = url.rpartition('/')[2]
@@ -82,17 +92,96 @@ def getFile(url: str, credentials: list):
     
     # Close the Connection
     ftp_server.quit()
+    return filename
+
+def getChildOfParentMap(parentMap: dict[ET._Element, ET._Element], name: str | ET._Element):
+    for child in parentMap.keys():
+        if child.tag == (name.tag if isinstance(name, ET._Element) else name):
+            return child
+
+def getParentOfParentMap(parentMap: dict[ET._Element, ET._Element], name: str | ET._Element):
+    for child, parent in parentMap.items():
+        if child.tag == (name.tag if isinstance(name, ET._Element) else name):
+            return parent
+
+def getParentMapOfXMLTree(xmlTree: ET.ElementTree):
+    return {child: parent for parent in xmlTree.getroot().iter() for child in parent} # safes map of childs and their parents in tree
+
+def appendChildToParent(parent: ET._Element, child: ET._Element):
+    if child is not None and parent is not None:
+        parent.append(child)
+        return child
+    else:
+        print("Parent or child is null.")
+
+def getHeaders(headerFileName: str='headers.txt'):
+    getFile(HOSTNAME + INTERVALL_PATH + headerFileName, CREDENTIALS)
+
+    with open(headerFileName, 'r') as headerFile:
+        headerReader = csv.reader(headerFile, delimiter=' ')
+        for i, line in enumerate(headerReader):
+            if i == 1:
+                line.pop() # last element of list is always empty
+                return line
+
+def putDataToXMLElement(xmlTree: ET._Element, elementName: str, data: str):
+    for element in xmlTree.iter(elementName):
+        element.text = data
+
+def putListOfDataToXML(xmlElement: ET._Element, headers: list[str], headersXMLMapping: dict[str,str], data: list[str] | str, delimiter: str=' '):
+    if isinstance(data, str):
+        data = delimiter.join(data.split())
+        data = data.split()
+    for j, value in enumerate(data):
+        if j < headers.__len__():
+            xmlElementTag = headersXMLMapping.get(headers[j])
+            if xmlElementTag and xmlElement is not None:
+                putDataToXMLElement(xmlElement, xmlElementTag, value)
+        
+def putDataToXML(pathToXMLTemplate: str, pathToData: str, mapping: dict[str,str], childElementsTagToReplicate: str):
+    parser = ET.XMLParser(encoding='UTF-8', dtd_validation=False, remove_comments=False, remove_pis=False)
+    xmlTree = ET.parse(source=pathToXMLTemplate, parser=parser)
+    headers = getHeaders()
+    parentMap = getParentMapOfXMLTree(xmlTree)
+    parent = getParentOfParentMap(parentMap, childElementsTagToReplicate)
+    elementToReplicate = deepcopy(getChildOfParentMap(parentMap, childElementsTagToReplicate))
+
+    with open(pathToData, 'r') as dataFile:
+        firstLine = dataFile.readline()
+
+        # put first line of the data file to childElementsTagToReplicate parent element + its child to replicate
+        putListOfDataToXML(parent, headers, mapping, firstLine)
+
+        # put the remaining lines of the data file to the xml and replicate the childElementsTagToReplicate element and append it to its parent
+        for line in dataFile:
+            child = deepcopy(elementToReplicate)
+            putListOfDataToXML(child, headers, mapping, line)
+            appendChildToParent(parent, child)
+
+    fileName = ''
+    if pathToXMLTemplate.rpartition('/')[2]:
+        fileName = pathToXMLTemplate.rpartition('/')[2].rpartition('.')[0] + '_filled.' + pathToXMLTemplate.rpartition('.')[2]
+    else:
+        fileName = pathToXMLTemplate.rpartition('.')[0] + '_filled.' + pathToXMLTemplate.rpartition('.')[2]
+
+    xmlTree.write(fileName, xml_declaration=True, encoding='UTF-8', pretty_print=True)
+    
 
 def main():
     args = cliArgumentParser()
+    setTimeIntervall(args.time_intervall)
     
     finalUrl = getUrl(args.time_intervall, args.year, args.location, '.txt', CREDENTIALS)
 
     print("Following file was found: '" + finalUrl + "'\n\nStarting download ...\n")
 
-    getFile(finalUrl, CREDENTIALS)
+    fileName = getFile(finalUrl, CREDENTIALS)
 
-    print("Download of file finished.\n\n")
+    print("Download of file finished.\n\nStart putting data to given XML template.\n")
+
+    putDataToXML("resources/weather-data.xml", fileName, HEADER_HOURLY_MAPPING, 'UTC_Date')
+
+    print("Putting data to XML template finished.")
 
 if __name__ == "__main__":
     main()
