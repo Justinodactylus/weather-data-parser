a261ffbc063548f1d85dc58365819fce08b7f390
Justin Kreller <justin.kreller@stud.htwk-leipzig.de>
2023-01-26 15:18:28 +0100

    add new location vector endpoint

--- src/api.py
+++ src/api.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import Response
 from flask_cors import CORS
 from main import apiMain
+import json
 import csv
 import os
 
@@ -11,8 +12,8 @@ CORS(app)
 
 STATIONS_TSV_LOCATION = "resources/stations.tsv"
 
-def getLocationVectorList(locationVectorList: str) -> list:
-    with open(locationVectorList) as tsv_file:
+def getLocationVectorList() -> list:
+    with open(STATIONS_TSV_LOCATION) as tsv_file:
         stations_reader = csv.reader(tsv_file, delimiter="\t")
 
         station_vector_list = []
@@ -27,11 +28,18 @@ def validateRequestParams(intervall: str, year: int, location: str) -> bool:
     if year not in range(2000, 2023):
         raise Exception("Given year has to be in the range of 2000 to 2023")
 
-    locationVectorList = getLocationVectorList(STATIONS_TSV_LOCATION)
+    locationVectorList = getLocationVectorList()
     if location not in locationVectorList:
         raise Exception("Given location must be a valid location vector. See station.tsv in the repo.")
 
     return True
+
+@app.route('/locationVectors/')
+def getLocationVectors():
+    vectorList = getLocationVectorList()
+    response = Response(json.dumps(vectorList), mimetype='application/json')
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/<string:intervall>/<int:year>/<string:location>/')
 def getWeatherData(intervall, year, location):
@@ -44,10 +52,10 @@ def getWeatherData(intervall, year, location):
         return str('Bad request: {}<br><br>Usage path: /string:intervall/int:year/string:location/').format(str(err)), 400
     
     filledXML = apiMain(intervall, year, location)
-    resp = Response(filledXML, mimetype='application/xml')
-    resp.headers["Content-Type"] = "text/xml; charset=utf-8"
-    resp.headers.add('Access-Control-Allow-Origin', '*')
-    return resp
+    response = Response(filledXML, mimetype='application/xml')
+    response.headers["Content-Type"] = "text/xml; charset=utf-8"
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
     
 
 if __name__ == "__main__":
